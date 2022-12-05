519ad5d70d18b236fa0dee727a1837531de696bb
elias areta <eliasarata@gmail.com>
2022-12-05 02:26:44 +0200

    done

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.json({ msg: "Welcome to Elias Project" });
+    res.json({ msg: "Welcome to Elias ProjectASDDS" });
 })
 
 module.exports = router;
